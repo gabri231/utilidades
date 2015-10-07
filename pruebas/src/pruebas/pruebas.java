d340437e292136c74c939d6c2ff0ee8136b114cb
Gabriel Calle torrez <gabriel.calle92@gmail.com>
2015-10-07 18:01:55 +0200

    Update pruebas.java

--- pruebas/src/pruebas/pruebas.java
+++ pruebas/src/pruebas/pruebas.java
@@ -3,15 +3,7 @@ package pruebas;
 public class pruebas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Un comenario añadido
-		// Otro comentario mas
-		// Tercer comentario
-		// Comentario matador.
-		// COmentario para matar
-		// Qué pasa con esta opcion?
-		// Osea esto tambien
-		// Comentario mas
+		//http://www.javahispano.org/certificacion/2011/12/24/genericos-y-colecciones-parte-2-colecciones.html
 	}
 
 }
